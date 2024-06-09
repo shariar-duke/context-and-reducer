bba270d8eb3afc527447a73ab081be42e06e94b9
shariar-duke <shariarmahmud225@gmail.com>
2024-06-10 01:05:27 +0600

    context and reducer

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+
+export default function App() {
+  return (
+    <div>Contex and reudcer</div>
+  )
+}
---
+++ src/Section.jsx
@@ -0,0 +1,4 @@
+/* eslint-disable react/prop-types */
+export default function Section({ children }) {
+  return <section className="section">{children}</section>
+}
